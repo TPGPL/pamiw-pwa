06c17650f010d0e01ba4a7fa1f9a202079ea9823
TPGPL <TPGPL@users.noreply.github.com>
2023-11-13 23:25:40 +0100

    Use different collection type for books

--- Models/Author.cs
+++ Models/Author.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
-        public List<Book>? Books { get; set; }
+        public ICollection<Book> Books { get; set; }
     }
 }

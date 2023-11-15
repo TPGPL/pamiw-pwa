b21e1ab3ee049f53097381b49ff9cd7392aedb2b
TPGPL <TPGPL@users.noreply.github.com>
2023-11-15 19:57:54 +0100

    Add services to app builder

--- Program.cs
+++ Program.cs
@@ -9,5 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8081/api/") });
 builder.Services.AddSingleton<AuthorService>();
+builder.Services.AddSingleton<PublisherService>();
+builder.Services.AddSingleton<BookService>();
 
 await builder.Build().RunAsync();
